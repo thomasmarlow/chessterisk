41b7cbd1ac0324aae5123aab412a3446215dcf55
Thomas Marlow <thomasmarlow.ds@gmail.com>
2021-03-03 21:56:46 -0300

    added decent homepage; added cafecito link

--- server.py
+++ server.py
@@ -3,18 +3,18 @@ app = flask.Flask(__name__)
 
 @app.route('/home', methods=['GET'])
 def home():
-    return 'Home'
+    return flask.render_template('home.html')
 
-@app.route('/newgame', methods=['GET'])
-def new_game():
-    return 'New game'
+# @app.route('/newgame', methods=['GET'])
+# def new_game():
+#     return 'New game'
 
-@app.route('/game/<gameview_id>', methods=['GET', 'POST'])
-def game(gameview_id):
-    if flask.request.method == 'POST':
-        return 'POST to game'
-    else:
-        return flask.render_template('game.html')
+# @app.route('/game/<gameview_id>', methods=['GET', 'POST'])
+# def game(gameview_id):
+#     if flask.request.method == 'POST':
+#         return 'POST to game'
+#     else:
+#         return flask.render_template('game.html')
 
 @app.route('/test/<side>', methods=['GET', 'POST'])
 def test(side):
---
+++ static/js/home.js
@@ -0,0 +1,7 @@
+$(document).on('click', '#home-test-red', function () {
+    window.location.href = "/test/red";
+});
+
+$(document).on('click', '#home-test-blue', function () {
+    window.location.href = "/test/blue";
+});
