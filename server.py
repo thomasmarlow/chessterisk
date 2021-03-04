4958f12a91be2516046b9fc301271182d3c2fa88
Thomas Marlow <thomasmarlow.ds@gmail.com>
2021-03-03 22:49:17 -0300

    added redirect to home if /test/* other than red/blue - might be better to raise 404 or sth

--- server.py
+++ server.py
@@ -21,6 +21,8 @@ def test(side):
     if flask.request.method == 'POST':
         return 'POST to game'
     else:
+        if not (side in ['red', 'blue']):
+            return flask.redirect(flask.url_for('home'))
         return flask.render_template('game.html', side=side)
 
 @app.errorhandler(404)
