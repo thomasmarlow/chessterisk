740158ce63b371b11a8918d791fb3e1ca6a91c50
Thomas Marlow <thomasmarlow.ds@gmail.com>
2021-03-08 03:24:19 -0300

    added std output confirmation to db wipe scripts

--- create_tables.py
+++ create_tables.py
@@ -2,3 +2,5 @@ from server import db, app
 
 with app.app_context():
     db.create_all()
+
+print('db.create_all() run!')
--- drop_tables.py
+++ drop_tables.py
@@ -2,3 +2,5 @@ from server import db, app
 
 with app.app_context():
     db.drop_all()
+
+print('db.drop_all() run!')
